fdee5c14318bb8a579f0486d597b06959126faea
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-28 16:51:36 +0530

    num.c
    good

---
+++ num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char str[100]
+int i=0;f=1;
+clrscr()
+gets(str)
+for(i=0;i<2;i++)
+{
+i=i+1;
+}
+printf("\n enter the number of character in the string");
+for(i=0;i<1;i++)
+{
+i==1
+}
+}
